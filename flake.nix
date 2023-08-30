a59515b3dbf1df7eb2d468fa8e10622cc9b489f3
Abdallah Ali <amaali1991@gmail.com>
2023-08-30 13:03:51 +0300

    rmove un used inputs

--- flake.nix
+++ flake.nix
@@ -8,30 +8,6 @@
     # NixPkgs Unstable (nixos-unstable)
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    # macOS Support (master)
-    darwin.url = "github:lnl7/nix-darwin";
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
-
-    # Hardware Configuration
-    nixos-hardware.url = "github:nixos/nixos-hardware";
-
-    # Generate System Images
-    nixos-generators.url =
-      "github:nix-community/nixos-generators";
-    nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
-
-    # Comma
-    comma.url = "github:nix-community/comma";
-    comma.inputs.nixpkgs.follows = "nixpkgs";
-
-    # System Deployment
-    deploy-rs.url = "github:serokell/deploy-rs";
-    deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
-
-    # Run unpatched dynamically compiled binaries
-    nix-ld.url = "github:Mic92/nix-ld";
-    nix-ld.inputs.nixpkgs.follows = "nixpkgs";
-
   };
 
   outputs = { self, flake-utils, ... }@inputs:
